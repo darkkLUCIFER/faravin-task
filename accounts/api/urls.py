f74294c469c41367867af7b4b70c4291de24ad9e
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2023-09-19 23:39:11 +0330

    made LoginView

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from accounts.api.views import RegisterCreatorView, RegisterViewerView
+from accounts.api.views import RegisterCreatorView, RegisterViewerView, LoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/creator/', RegisterCreatorView.as_view(), name='register-creator'),
     path('register/viewer/', RegisterViewerView.as_view(), name='register-viewer'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from accounts.api.serializers import RegisterSerializer
 
@@ -28,7 +30,8 @@ class RegisterCreatorView(APIView):
                 return Response(
                     {
                         'errors': 'user exists'
-                    }
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
                 )
             else:
                 get_user_model().objects.create(
@@ -39,7 +42,8 @@ class RegisterCreatorView(APIView):
                 return Response(
                     {
                         'data': 'user created'
-                    }
+                    },
+                    status=status.HTTP_201_CREATED
                 )
         else:
             return Response(
@@ -68,7 +72,8 @@ class RegisterViewerView(APIView):
                 return Response(
                     {
                         'errors': 'user exists'
-                    }
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
                 )
             else:
                 get_user_model().objects.create(
@@ -79,9 +84,49 @@ class RegisterViewerView(APIView):
                 return Response(
                     {
                         'data': 'user created'
-                    }
+                    },
+                    status=status.HTTP_201_CREATED
                 )
         else:
             return Response(
                 serializer.errors
             )
+
+
+class LoginView(APIView):
+    """
+        post:
+            verify user and return access and refresh token
+
+            body: [username, password]
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.data.get('username')
+            password = serializer.data.get('password')
+
+            user = get_user_model().objects.get(username=username)
+
+            if check_password(password, user.password):
+                refresh = RefreshToken.for_user(user)
+
+                token = str(refresh.access_token)
+                refresh_token = str(refresh)
+
+                return Response(
+                    {
+                        'access': token,
+                        'refresh': refresh_token
+                    },
+                    status=status.HTTP_200_OK
+                )
+            else:
+                return Response(
+                    {
+                        'errors': 'invalid credentials'
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
